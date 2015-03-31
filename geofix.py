9ed43663f6d5c21327ac4398fcaa26695ad3ad04
Dmitri Popov <dmpop@linux.com>
2015-03-31 21:55:21 +0200

    thinkpad-t410-kubuntu

--- geofix.py
+++ geofix.py
@@ -25,11 +25,11 @@ try:
     net_lat = net['latitude']
     net_lon = net['longitude']
     OSM='http://www.openstreetmap.org/index.html?mlat=' + str(net_lat) + '&mlon=' + str(net_lon) + '&zoom=18'
-    droid.makeToast('Network: ' + str(net_lat) + ' ' + str(net_lon))
     f_path = '/sdcard/geofix_net.tsv'
     f = open(f_path,'a')
     f.write(str(date_stamp) + '\t' + str(time_stamp) + '\t' + str(net_lat) + '\t' + str(net_lon) + '\t' + OSM + '\n')
     f.close()
+    droid.makeToast('Network: ' + str(net_lat) + ' ' + str(net_lon))
 except (KeyError):
     droid.makeToast('Failed. Please try again.')
     sys.exit()
@@ -39,11 +39,11 @@ try:
     gps_lat = gps['latitude']
     gps_lon = gps['longitude']
     OSM='http://www.openstreetmap.org/index.html?mlat=' + str(gps_lat) + '&mlon=' + str(gps_lon) + '&zoom=18'
-    droid.makeToast('GPS: ' + str(net_lat) + ' ' + str(net_lon))
     f_path = '/sdcard/geofix_gps.tsv'
     f = open(f_path,'a')
     f.write(str(date_stamp) + '\t' + str(time_stamp) + '\t' + str(gps_lat) + '\t' + str(gps_lon) + '\t' + OSM + '\n')
     f.close()
+    droid.makeToast('GPS: ' + str(net_lat) + ' ' + str(net_lon))
 except (KeyError):
     droid.makeToast('Failed. Please try again.')
     sys.exit()
