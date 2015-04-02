1e1c5b509a0458f29dc654d2b6aa58bfdf96e3c2
Dmitri Popov <dmpop@linux.com>
2015-04-02 18:16:18 +0200

    thinkpad-t410-kubuntu :coffee:

--- geofix.py
+++ geofix.py
@@ -1,6 +1,12 @@
 import android, time, sys
 import os, sqlite3
 from datetime import datetime
+
+geofix_dir = '/sdcard/Geofix/'
+
+if not os.path.exists(geofix_dir):
+    os.makedirs(geofix_dir)
+
 droid = android.Android()
 droid.startLocating()
 time.sleep(9)
@@ -14,7 +20,7 @@ try:
     net_lat = net['latitude']
     net_lon = net['longitude']
     OSM='http://www.openstreetmap.org/index.html?mlat=' + str(net_lat) + '&mlon=' + str(net_lon) + '&zoom=18'
-    f_path = '/sdcard/geofix_net.tsv'
+    f_path = geofix_dir + 'geofix_net.tsv'
     f = open(f_path,'a')
     f.write(str(date_stamp) + '\t' + str(time_stamp) + '\t' + str(net_lat) + '\t' + str(net_lon) + '\t' + OSM + '\n')
     f.close()
@@ -27,23 +33,23 @@ try:
     gps_lat = gps['latitude']
     gps_lon = gps['longitude']
     OSM='http://www.openstreetmap.org/index.html?mlat=' + str(gps_lat) + '&mlon=' + str(gps_lon) + '&zoom=18'
-    f_path = '/sdcard/geofix_gps.tsv'
+    f_path = geofix_dir + 'geofix_gps.tsv'
     f = open(f_path,'a')
     f.write(str(date_stamp) + '\t' + str(time_stamp) + '\t' + str(gps_lat) + '\t' + str(gps_lon) + '\t' + OSM + '\n')
     f.close()
     droid.notify('GPS coordinates', str(gps_lat) + ' ' + str(gps_lon))
     #Save the obtained data in the geofix.sqlite database
-    if os.path.exists('geofix.sqlite'):
+    if os.path.exists(geofix_dir + 'geofix.sqlite'):
         sql_query = "INSERT INTO geofix (d_stamp, t_stamp, gps_lat, gps_lon, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s')" % (date_stamp, time_stamp, gps_lat, gps_lon, OSM)
-        conn = sqlite3.connect('geofix.sqlite')
+        conn = sqlite3.connect(geofix_dir + 'geofix.sqlite')
         conn.execute(sql_query)
         conn.commit()
         conn.close()
     else:
-        conn = sqlite3.connect('geofix.sqlite')
+        conn = sqlite3.connect(geofix_dir + 'geofix.sqlite')
         conn.execute("CREATE TABLE geofix (id INTEGER PRIMARY KEY, d_stamp char(10), t_stamp char(8), gps_lat char(11), gps_lon char(11), osm_url char(256))")
         sql_query = "INSERT INTO geofix (d_stamp, t_stamp, gps_lat, gps_lon, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s')" % (date_stamp, time_stamp, gps_lat, gps_lon, OSM)
-        conn = sqlite3.connect('geofix.sqlite')
+        conn = sqlite3.connect(geofix_dir + 'geofix.sqlite')
         conn.execute(sql_query)
         conn.commit()
         conn.close()
