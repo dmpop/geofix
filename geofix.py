104500b932fb2310aaa678396b87fffdb600f678
Dmitri Popov <dmpop@linux.com>
2015-04-02 15:57:52 +0200

    dn2820fykh-kubuntu :coffee:

--- geofix.py
+++ geofix.py
@@ -1,4 +1,5 @@
 import android, time, sys
+import os, sqlite3
 from datetime import datetime
 droid = android.Android()
 droid.startLocating()
@@ -17,7 +18,7 @@ try:
     f = open(f_path,'a')
     f.write(str(date_stamp) + '\t' + str(time_stamp) + '\t' + str(net_lat) + '\t' + str(net_lon) + '\t' + OSM + '\n')
     f.close()
-    droid.makeToast('Network: ' + str(net_lat) + ' ' + str(net_lon))
+    droid.notify('Network coordinates', str(net_lat) + ' ' + str(net_lon))
 except (KeyError):
     droid.makeToast('Failed. Please try again.')
 
@@ -30,7 +31,22 @@ try:
     f = open(f_path,'a')
     f.write(str(date_stamp) + '\t' + str(time_stamp) + '\t' + str(gps_lat) + '\t' + str(gps_lon) + '\t' + OSM + '\n')
     f.close()
-    droid.makeToast('GPS: ' + str(gps_lat) + ' ' + str(gps_lon))
+    droid.notify('GPS coordinates', str(gps_lat) + ' ' + str(gps_lon))
+    #Save the obtained data in the geofix.sqlite database
+    if os.path.exists('geofix.sqlite'):
+        sql_query = "INSERT INTO geofix (d_stamp, t_stamp, gps_lat, gps_lon, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s')" % (date_stamp, time_stamp, gps_lat, gps_lon, OSM)
+        conn = sqlite3.connect('geofix.sqlite')
+        conn.execute(sql_query)
+        conn.commit()
+        conn.close()
+    else:
+        conn = sqlite3.connect('geofix.sqlite')
+        conn.execute("CREATE TABLE geofix (id INTEGER PRIMARY KEY, d_stamp char(10), t_stamp char(8), gps_lat char(11), gps_lon char(11), osm_url char(256))")
+        sql_query = "INSERT INTO geofix (d_stamp, t_stamp, gps_lat, gps_lon, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s')" % (date_stamp, time_stamp, gps_lat, gps_lon, OSM)
+        conn = sqlite3.connect('geofix.sqlite')
+        conn.execute(sql_query)
+        conn.commit()
+        conn.close()
     #Uncomment to show the obtained GPS coordinates on OpenStreetMap
     # droid.dialogCreateAlert("OpenStreetMap","Show location on the map?")
     # droid.dialogSetPositiveButtonText("Yes")
@@ -45,5 +61,3 @@ try:
 except (KeyError):
     droid.makeToast('Failed. Please try again.')
     sys.exit()
-
-droid.notify('Geofix', 'All done!')
