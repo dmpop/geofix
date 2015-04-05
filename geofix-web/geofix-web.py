f9af7a1f38794446a78a4d342c375a4e7ea3d5be
Dmitri Popov <dmpop@linux.com>
2015-04-05 13:13:18 +0200

    thinkpad-t410-kubuntu :lollipop:

--- geofix-web/geofix-web.py
+++ geofix-web/geofix-web.py
@@ -7,7 +7,7 @@ def wimb():
     if os.path.exists('geofix.sqlite'):
         conn = sqlite3.connect('geofix.sqlite')
         c = conn.cursor()
-        c.execute("SELECT * FROM geofix ASC")
+        c.execute("SELECT * FROM geofix ORDER BY d_stamp DESC")
         result = c.fetchall()
         c.close()
         output = template('geofix.tpl', rows=result)
