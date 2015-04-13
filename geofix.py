833461c9c335547620d5a86634c4f4f07bd4cf7a
Dmitri Popov <dmpop@linux.com>
2015-04-13 17:59:50 +0200

    thinkpad-t410-kubuntu :coffee:

--- geofix.py
+++ geofix.py
@@ -37,15 +37,17 @@ except (KeyError):
     except (KeyError):
         droid.makeToast('Geofix failed to obtain coordinates.')
         sys.exit()
+#Generate coordinates in the digiKam format
+digikam = 'geo' + lat + ',' + lon
 #Generate an OpenStreetMap URL and save the prepared data in the geofix.tsv file
 osm ='http://www.openstreetmap.org/index.html?mlat=' + str(lat) + '&mlon=' + str(lon) + '&zoom=18'
 f_path = geofix_dir + 'geofix.tsv'
 f = open(f_path,'a')
-f.write(str(dt) + '\t' + str(lat) + '\t' + str(lon) + '\t' + place + '\t' + osm + '\n')
+f.write(str(dt) + '\t' + str(lat) + '\t' + str(lon) + '\t' + digikam + '\t' + place + '\t' + osm + '\n')
 f.close()
 #Save the prepared data in the geofix.sqlite database
 if os.path.exists(geofix_dir + 'geofix.sqlite'):
-    sql_query = "INSERT INTO geofix (dt, lat, lon, place, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s')" % (dt, lat, lon, place, osm)
+    sql_query = "INSERT INTO geofix (dt, lat, lon, digikam, place, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (dt, lat, lon, digikam, place, osm)
     conn = sqlite3.connect(geofix_dir + 'geofix.sqlite')
     conn.execute(sql_query)
     conn.commit()
@@ -53,8 +55,8 @@ if os.path.exists(geofix_dir + 'geofix.sqlite'):
 else:
     #Create the database if it doesn't exist
     conn = sqlite3.connect(geofix_dir + 'geofix.sqlite')
-    conn.execute("CREATE TABLE geofix (id INTEGER PRIMARY KEY, dt VARCHAR, lat VARCHAR, lon VARCHAR, place VARCHAR, osm_url VARCHAR)")
-    sql_query = "INSERT INTO geofix (dt, lat, lon, place, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s')" % (dt, lat, lon, place, osm)
+    conn.execute("CREATE TABLE geofix (id INTEGER PRIMARY KEY, dt VARCHAR, lat VARCHAR, lon VARCHAR, digiKam VARCHAR, place VARCHAR, osm_url VARCHAR)")
+    sql_query = "INSERT INTO geofix (dt, lat, lon, digikam, place, osm_url) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (dt, lat, lon, digikam, place, osm)
     conn = sqlite3.connect(geofix_dir + 'geofix.sqlite')
     conn.execute(sql_query)
     conn.commit()
