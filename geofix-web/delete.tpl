c3fc772ab6592616b790618420357089b29a6ff5
Dmitri Popov <dmpop@linux.com>
2015-05-17 10:12:06 +0200

    thinkpad-t410-kubuntu :coffee:

---
+++ geofix-web/delete.tpl
@@ -0,0 +1,16 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../static/styles.css">
+<link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oxygen+Mono' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<title>Geofix</title>
+</head>
+<title>Geofix</title>
+<div id="content">
+<h2>Delete {{no}}?</h2>
+<form action="/delete/{{no}}" method="GET">
+<input type="submit" id="btn" class="warning" name="delete" value="Delete">
+</form>
+<p><a href="/geofix">Back</a></p>
+</div>
--- geofix-web/main.py
+++ geofix-web/main.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+
+#!/usr/bin/python
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sqlite3, os
 from bottle import route, run, debug, template, request, redirect, static_file
 
@@ -10,7 +26,7 @@ def geofix():
         c.execute("SELECT * FROM geofix ORDER BY dt DESC")
         result = c.fetchall()
         c.close()
-        output = template('geofix.tpl', rows=result)
+        output = template('main.tpl', rows=result)
         return output
     else:
         return ('The geofix.sqlite database is not found')
@@ -25,6 +41,20 @@ def snapshot(dt):
     c = conn.cursor()
     c.execute("SELECT * FROM geofix WHERE dt LIKE ?", (dt, ))
     conn.commit()
-    return template('geofix-snapshot.tpl', dt=dt)
+    return template('snapshot.tpl', dt=dt)
+
+@route('/delete/:no', method='GET')
+def delete(no):
+
+    if request.GET.get('delete','').strip():
+        conn = sqlite3.connect('static/Geofix/geofix.sqlite')
+        c = conn.cursor()
+        c.execute("DELETE FROM geofix WHERE dt LIKE ?", (no, ))
+        conn.commit()
+        os.remove('static/Geofix/' + no + '.jpg')
+
+        return redirect('/geofix')
+    else:
+        return template('delete.tpl', no=no)
 
 run(host="0.0.0.0",port=8381, debug=True, reloader=True)
